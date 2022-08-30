e1373eae417fd9bcc6e82ef0dae9e62492e5b3d4
anarhitetka <a.urumovska@gmail.com>
2022-08-30 15:55:22 +0200

    Enhancement: add styled-components dependency

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -1,3 +1,9 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+  border: 1px solid red;
+`;
+
 export default function TeamCard({ teamInfo }) {
   return (
     <>
@@ -15,30 +21,32 @@ export default function TeamCard({ teamInfo }) {
             venue,
           } = team;
           return (
-            <div key={alternateIds.sdr}>
-              <p style={{ backgroundColor: "#" + color }}>.</p>
-              <p style={{ backgroundColor: "#" + alternateColor }}>.</p>
-              <h3>
-                {abbreviation}: {displayName} ({team.nickname})
-              </h3>
-              <p>
-                <strong>Location:</strong> {location}
-              </p>
-              <img src={logos[0].href} height="100" alt="team logo" />
-              <p>
-                <strong>Venue:</strong>
-                <br />
-                {venue.fullName} <br />
-                capacity: {venue.capacity} <br />
-                indoor: {venue.indoor ? "yes" : "no"} <br />
-                grass: {venue.grass ? "yes" : "no"}
-              </p>
-              {venue.images.map((image) => {
-                return <img src={image.href} height="200" alt="venue" />;
-              })}
+            <Card>
+              <div key={alternateIds.sdr}>
+                <p style={{ backgroundColor: "#" + color }}>.</p>
+                <p style={{ backgroundColor: "#" + alternateColor }}>.</p>
+                <h3>
+                  {abbreviation}: {displayName} ({team.nickname})
+                </h3>
+                <p>
+                  <strong>Location:</strong> {location}
+                </p>
+                <img src={logos[0].href} height="100" alt="team logo" />
+                <p>
+                  <strong>Venue:</strong>
+                  <br />
+                  {venue.fullName} <br />
+                  capacity: {venue.capacity} <br />
+                  indoor: {venue.indoor ? "yes" : "no"} <br />
+                  grass: {venue.grass ? "yes" : "no"}
+                </p>
+                {venue.images.map((image) => {
+                  return <img src={image.href} height="200" alt="venue" />;
+                })}
 
-              <hr />
-            </div>
+                <hr />
+              </div>
+            </Card>
           );
         })}
       </div>
