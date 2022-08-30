b73ddbf4d82d6039150e7e71aaeba0af55f8d83f
anarhitetka <a.urumovska@gmail.com>
2022-08-30 15:50:44 +0200

    Improvement: destructure prop

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -3,26 +3,37 @@ export default function TeamCard({ teamInfo }) {
     <>
       <div>
         {teamInfo.map((team) => {
+          const {
+            abbreviation,
+            displayName,
+            color,
+            alternateColor,
+            location,
+            logos,
+            // id,
+            alternateIds,
+            venue,
+          } = team;
           return (
-            <div key={team.alternateIds.sdr}>
-              <p style={{ backgroundColor: "#" + team.color }}>.</p>
-              <p style={{ backgroundColor: "#" + team.alternateColor }}>.</p>
+            <div key={alternateIds.sdr}>
+              <p style={{ backgroundColor: "#" + color }}>.</p>
+              <p style={{ backgroundColor: "#" + alternateColor }}>.</p>
               <h3>
-                {team.abbreviation}: {team.displayName} ({team.nickname})
+                {abbreviation}: {displayName} ({team.nickname})
               </h3>
               <p>
-                <strong>Location:</strong> {team.location}
+                <strong>Location:</strong> {location}
               </p>
-              <img src={team.logos[0].href} height="100" alt="team logo" />
+              <img src={logos[0].href} height="100" alt="team logo" />
               <p>
                 <strong>Venue:</strong>
                 <br />
-                {team.venue.fullName} <br />
-                capacity: {team.venue.capacity} <br />
-                indoor: {team.venue.indoor ? "yes" : "no"} <br />
-                grass: {team.venue.grass ? "yes" : "no"}
+                {venue.fullName} <br />
+                capacity: {venue.capacity} <br />
+                indoor: {venue.indoor ? "yes" : "no"} <br />
+                grass: {venue.grass ? "yes" : "no"}
               </p>
-              {team.venue.images.map((image) => {
+              {venue.images.map((image) => {
                 return <img src={image.href} height="200" alt="venue" />;
               })}
 
