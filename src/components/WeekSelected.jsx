c0d46ee6bdf6a76db032a0f9d286903aa6382a0f
anarhitetka <a.urumovska@gmail.com>
2022-11-21 20:39:00 +0100

    Improvement: get team summary data(Team) and teams on bye (Weeks)

--- src/components/WeekSelected.jsx
+++ src/components/WeekSelected.jsx
@@ -15,28 +15,50 @@ S.GamesContainer = styled.div`
 export default function WeekSelected({ weekNo }) {
 
   const allGamesData = ApiCalls.getEventsForWeek(weekNo);
+  const teamsOnBye = ApiCalls.getTeamsOnByeForWeekNo(weekNo);
+  console.log(teamsOnBye);
 
   // TODO: group games by day of the week
 
   return (
     <>
-      {allGamesData.isLoading ? (
-        <CircularProgress />
-      ) : (
-        <S.GamesContainer>
-          {allGamesData.data &&
-            allGamesData.data.map((event) => {
-              return (
-                <Game
-                  event={event}
-                  key={`${event.id}-${Math.random().toString()}`}
-                  weekNo={weekNo}
-                  type="details"
-                />
-              );
-            })}
-        </S.GamesContainer>
-      )}
+      <div>
+        {/* Each team has one bye week between Weeks 6 and 14 (regular season) */}
+        {weekNo >= 6 && weekNo <= 14 ? (
+          teamsOnBye.isLoading ? (
+            <CircularProgress />
+          ) : (
+            <div style={{ textAlign: "center" }} >
+              <span>TEAMS ON BYE (week {weekNo}): </span>
+              {teamsOnBye.data.map((team, i, arr) => {
+                return (
+                  <span key={`team-on-bye-${team.id}`} >{team.displayName}{i !== arr.length - 1 ? ', ' : ''} </span>
+                )
+              })}
+            </div>
+          )
+        ) : "no teams are on bye for the selected week"}
+      </div>
+
+      {
+        allGamesData.isLoading ? (
+          <CircularProgress />
+        ) : (
+          <S.GamesContainer>
+            {allGamesData.data &&
+              allGamesData.data.map((event) => {
+                return (
+                  <Game
+                    event={event}
+                    key={`${event.id}-${Math.random().toString()}`}
+                    weekNo={weekNo}
+                    type="details"
+                  />
+                );
+              })}
+          </S.GamesContainer>
+        )
+      }
     </>
   );
 }
