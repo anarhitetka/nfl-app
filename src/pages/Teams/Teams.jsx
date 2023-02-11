6b231d41c65c965165bfd0ee8eb9993d26ac9031
anarhitetka <a.urumovska@gmail.com>
2023-02-11 17:30:04 +0100

    Style refactor Teams page

--- src/pages/Teams/Teams.jsx
+++ src/pages/Teams/Teams.jsx
@@ -3,58 +3,65 @@ import * as S from "./Teams.styled.js";
 
 export default function Teams() {
 
-  const allTeamsByGroup = [
-    // American Football Conference
-    { id: 'group-afc-east', groupName: 'AFC East', teams: ApiCalls.getAfcEastTeams().data },
-    { id: 'group-afc-north', groupName: 'AFC North', teams: ApiCalls.getAfcNorthTeams().data },
-    { id: 'group-afc-south', groupName: 'AFC South', teams: ApiCalls.getAfcSouthTeams().data },
-    { id: 'group-afc-west', groupName: 'AFC West', teams: ApiCalls.getAfcWestTeams().data },
-    // National Football Conference
-    { id: 'group-nfc-east', groupName: 'NFC East', teams: ApiCalls.getNfcEastTeams().data },
-    { id: 'group-nfc-north', groupName: 'NFC North', teams: ApiCalls.getNfcNorthTeams().data },
-    { id: 'group-nfc-south', groupName: 'NFC South', teams: ApiCalls.getNfcSouthTeams().data },
-    { id: 'group-nfc-west', groupName: 'NFC West', teams: ApiCalls.getNfcWestTeams().data },
+  const allTeamsByGroupAndConference = [
+    {
+      id: 'conf-afc', groups: [
+        // American Football Conference
+        { id: 'group-afc-east', groupName: 'AFC East', teams: ApiCalls.getAfcEastTeams().data },
+        { id: 'group-afc-north', groupName: 'AFC North', teams: ApiCalls.getAfcNorthTeams().data },
+        { id: 'group-afc-south', groupName: 'AFC South', teams: ApiCalls.getAfcSouthTeams().data },
+        { id: 'group-afc-west', groupName: 'AFC West', teams: ApiCalls.getAfcWestTeams().data },
+      ]
+    },
+    {
+      id: 'conf-nfc', groups: [
+        // National Football Conference
+        { id: 'group-nfc-east', groupName: 'NFC East', teams: ApiCalls.getNfcEastTeams().data },
+        { id: 'group-nfc-north', groupName: 'NFC North', teams: ApiCalls.getNfcNorthTeams().data },
+        { id: 'group-nfc-south', groupName: 'NFC South', teams: ApiCalls.getNfcSouthTeams().data },
+        { id: 'group-nfc-west', groupName: 'NFC West', teams: ApiCalls.getNfcWestTeams().data },
+      ]
+    },
   ];
 
   return (
     <>
-      <S.TeamsContainer>
-
-        {allTeamsByGroup.map(group => {
-
+      <S.AllTeamsContainer>
+        {allTeamsByGroupAndConference.map(conf => {
           return (
-            <S.DivisionContainer key={group.id}>
-
-              <S.DivisionHeading>{group.groupName} Teams</S.DivisionHeading>
-
-              <S.DivisionTeams>
-
-                {group.teams.map(team => {
-
-                  const { abbreviation, displayName, color, logos, id, alternateIds } =
-                    team;
-
-                  return (
-                    <S.TeamCard key={alternateIds.sdr}>
-                      <S.TeamLink to={`/teams/${id}`}>
-                        <p style={{ backgroundColor: "#" + color, color: "white" }}>
-                          {abbreviation}
-                        </p>
-                        <img src={logos[0].href} height="80" alt="team logo" />
-                        <S.HeadingH4>{displayName}</S.HeadingH4>
-                      </S.TeamLink>
-                    </S.TeamCard>
-                  )
-
-                })}
-
-              </S.DivisionTeams>
-
-            </S.DivisionContainer>
+            // CONFERENCES
+            <S.Conference key={conf.id}>
+              {conf.groups.map(group => {
+                // GROUPS
+                return (
+                  <S.Division key={group.id}>
+                    <S.DivisionName>{group.groupName}</S.DivisionName>
+                    {/* TEAMS  */}
+                    {group.teams.map(team => {
+                      const { displayName, logos, alternateIds, venue, id } =
+                        team;
+                      return (
+                        <S.TeamDiv key={alternateIds.sdr}>
+                          <S.TeamNavLink to={`/teams/${id}`}>
+                            <img src={logos[0].href} height="70" alt="team logo" className="logo-img" />
+                            <div className="team-text">
+                              <p className="team-display-name">{displayName}</p>
+                              <div className="team-venue">
+                                <p>{venue.fullName}</p>
+                                <p>{venue.address.city}</p>
+                              </div>
+                            </div>
+                          </S.TeamNavLink>
+                        </S.TeamDiv>
+                      )
+                    })}
+                  </S.Division>
+                )
+              })}
+            </S.Conference>
           )
         })}
-
-      </S.TeamsContainer>
+      </S.AllTeamsContainer>
     </>
   );
 }
--- src/pages/Teams/Teams.styled.js
+++ src/pages/Teams/Teams.styled.js
@@ -1,76 +1,114 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-export const TeamsContainer = styled.div`
+export const AllTeamsContainer = styled.div`
   box-sizing: border-box;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: center;
+  background: #B5BFCA;
+  background: -webkit-linear-gradient(bottom right, #B5BFCA, #EEEEEE);
+  background: -moz-linear-gradient(bottom right, #B5BFCA, #EEEEEE);
+  background: linear-gradient(to top left, #B5BFCA, #EEEEEE);
 `;
 
-export const DivisionContainer = styled.div`
-  border-radius: 3px;
-  display: flex; 
-  flex-direction: column; 
-  align-items: center;
-  margin: 15px;
-  box-shadow: 4px 5px 23px 4px black;
-  background-color: white;
+export const Conference = styled.div`
+  width: 48vw;
+  @media (max-width: 800px) {
+    width: 96vw;
+  }
 `;
 
-export const DivisionHeading = styled.h4`
+export const Division = styled.div`
   display: flex;
-  align-items: center;
-  font-size: 20px;
-  font-weight: 600;
-  color: #013369;
-  margin: 0;
-  padding: 5px;
-`;
-
-export const DivisionTeams = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: center;
-`;
-
-export const TeamCard = styled.div`
-  box-sizing: border-box;
-  border: 1px solid grey;
-  width: 120px;
+  flex-direction: column;
   margin: 10px;
-  text-align: center;
-  display: flex;
-  flex-direction: column;
 `;
 
-export const HeadingH4 = styled.h4`
+export const DivisionName = styled.h4`
+  display: flex;
+  align-items: center;
+  font-size: 17px;
+  font-weight: 900;
+  color: #013369;
+  margin: 10px;
   padding: 5px;
-  height: 50px;
-  display: flex;
-  align-items: center;
 `;
 
-export const TeamLink = styled(Link)`
-  box-sizing: border-box;
-  border: 3px solid transparent;
+export const TeamDiv = styled.div`
+  background-color: #fff;
+  border-radius: 3px;
+  margin: 0 10px 10px 10px;
   display: flex;
-  align-items: center;
-  flex-direction: column;
-  justify-content: space-between;
-  height: 100%;
+  flex-direction: row;
+  .logo-img {
+    padding: 10px;
+  }
+  .team-text {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  .team-display-name {
+    margin: 0;
+    padding: 5px 10px;
+    font-size: 17px;
+    font-weight: 700;
+  }
+  .team-venue {
+    color: grey;
+    font-size: 12px;
+    p {
+      margin: 0;
+      padding: 0 10px;
+    }
+  }
+
+  @media (max-width: 400px) {
+    .logo-img {
+      height: 50px;
+    }
+    .team-display-name {
+      font-size: 15px;
+    }
+    .team-venue {
+      font-size: 11px;
+    }
+  }
+  @media (max-width: 300px) {
+    .logo-img {
+      height: 35px;
+    }
+    .team-display-name {
+      font-size: 5vw;
+    }
+    .team-venue {
+      display: none;
+    }
+  }
+  @media (max-width: 170px) {
+      justify-content: center;
+    .team-display-name {
+      display: none;
+    }
+  }
+`;
+
+export const TeamNavLink = styled(Link)`
+  display: flex;
+  width: 100%;
   text-decoration: none;
   color: black;
+  box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.3); 
+
   &:hover,
   &:focus {
-    border: 3px solid #013369;
+    color: #013369;
+    box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.5); 
   }
   &:active {
-    color: grey;
-  }
-  p {
-    width: 100%;
+    color: #013369;
+    box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.2); 
   }
 `;
