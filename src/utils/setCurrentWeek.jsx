ebf21b1dde6e0c3094add5843f4af1136829416e
anarhitetka <a.urumovska@gmail.com>
2023-02-15 20:10:19 +0100

    set current view in schedule to last week

--- src/utils/setCurrentWeek.jsx
+++ src/utils/setCurrentWeek.jsx
@@ -3,8 +3,9 @@ export function setCurrentWeekNo(weeksData) {
     return new Date() < new Date(week.endDate);
   });
   let weekNo;
+  let duration = weeksData.length > 0 ? weeksData.length : 1;
   // eslint-disable-next-line
-  if (current == -1) weekNo = 1;
+  if (current == -1) weekNo = duration;
   // eslint-disable-next-line
   else if (current != -1) weekNo = current + 1;
   return weekNo;
